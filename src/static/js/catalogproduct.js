9ad460730a35c3fd7b1ab0c7b8acd0ee6a94872e
bango <ke.malikov@yandex.ru>
2021-09-17 19:07:29 +0300

    Правки

--- src/static/js/catalogproduct.js
+++ src/static/js/catalogproduct.js
@@ -1,8 +1,8 @@
-new Swiper('.catalogproduct__block1-slider',{
-    
-    navigation:{
-        nextEl:'.swiper-button-next',
-        prevEl:'.swiper-button-prev'
+new Swiper('.catalogproduct__block1-slider', {
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
     },
     // pagination:{
     //     el:'.swiper-pagination ',
@@ -11,10 +11,10 @@ new Swiper('.catalogproduct__block1-slider',{
     simulateTouch: true,
     slidesPerView: 1,
     watchOverflow: true,
-    effect:'fade',
-    thumbs:{
-        swiper:{
-            el:'.catalogproduct__block1-slider-mini',
+    effect: 'fade',
+    thumbs: {
+        swiper: {
+            el: '.catalogproduct__block1-slider-mini',
             slidesPerView: 4,
             spaceBetween: 16,
         }
@@ -23,11 +23,28 @@ new Swiper('.catalogproduct__block1-slider',{
         el: ".swiper-pagination"
     }
 });
+new Swiper('.block_with_cards-element', {
+    simulateTouch: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        830: {
+            slidesPerView: 3,
+        },
+        900: {
+            slidesPerView: 4,
+        },
+    },
+    watchOverflow: true,
+})
 
-
-$( document ).ready(function() {
-    if ($('.catalogproduct__wrapp').length != 0){
-        $('.catalogproduct__block2-info-element').click(function (){
+$(document).ready(function () {
+    if ($('.catalogproduct__wrapp').length != 0) {
+        $('.catalogproduct__block2-info-element').click(function () {
             $(this).next().slideToggle()
             $(this).toggleClass('toggled')
         })
